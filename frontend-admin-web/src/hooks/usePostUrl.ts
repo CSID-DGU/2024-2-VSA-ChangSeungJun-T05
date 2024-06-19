a23da6d538770aa05aaf09cd35874e8d4df843f1
minseong0324 <kwan0324@naver.com>
2024-06-20 04:56:26 +0900

    Feat: add invalidateQueries

--- frontend-admin-web/src/hooks/usePostUrl.ts
+++ frontend-admin-web/src/hooks/usePostUrl.ts
@@ -1,8 +1,11 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postUrl } from '@/api/postUrl';
 import { TURLIds } from '@/types/urlList';
+import { getUrlListQueryKey } from '@/constants/queryKeys';
 
 export const usePostUrl = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (body: TURLIds) => postUrl(body),
     onSuccess: async () => {
@@ -11,5 +14,9 @@ export const usePostUrl = () => {
     onError: async () =>{
         alert("오류가 발생하였습니다.")
     },
+    onSettled: async () => {
+      queryClient.invalidateQueries({queryKey: getUrlListQueryKey().queryKey});
+
+    }
   });
 };
