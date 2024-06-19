dc6ec890d122d613962df5126fc6c7ec5e0fcdb0
minseong0324 <kwan0324@naver.com>
2024-06-20 04:49:25 +0900

    Feat: Authorization header add

--- frontend-admin-web/src/api/axios.ts
+++ frontend-admin-web/src/api/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: 'https://api.defend-url-admin.site',
   headers: {
-    Authorization: `Bearer `,
+    Authorization: `Bearer `, // TODO: 수정 필요
   },
 });
