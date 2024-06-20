66bd16d3490ed6aa5320baa9f6281d01044c7594
MINSEONG KIM <65233075+minseong0324@users.noreply.github.com>
2024-06-20 23:33:04 +0900

    🐛 Fix: fix admin web (#51)
    * Fix: fix api response
    
    * Fix: remove header
    
    * Fix: remove header

--- frontend-admin-web/src/api/axios.ts
+++ frontend-admin-web/src/api/axios.ts
@@ -2,7 +2,5 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'https://api.defend-url.site',
-  headers: {
-    Authorization: `Bearer `,
-  },
+  withCredentials: false,
 });
